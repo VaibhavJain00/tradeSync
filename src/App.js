c51e15b44bf541027a0ba0f8da30e2570aef74cf
VaibhavJain00 <vaibhavjain32003@gmail.com>
2023-11-15 12:52:43 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React 123
+          Learn React with tradesync
         </a>
       </header>
     </div>
