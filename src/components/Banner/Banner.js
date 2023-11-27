a7a94b04811d1fba88443b3941b6fb3291ff86d3
yashank123 <yashanklakhena@gmail.com>
2023-11-27 23:51:22 +0530

    commit

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,50 @@
+import { Container, Typography, makeStyles } from "@material-ui/core";
+import React from "react";
+
+const useStyles=makeStyles(()=> ({
+    banner: {
+        backgroundImage: "url(./banner2.jpg)",
+    },
+    bannerContent: {
+        height: 400,
+        display: "flex",
+        flexDirection: "column",
+        paddingTop: 25,
+        justifyContent: "space-around",
+    },
+}));
+
+const Banner = () => {
+    const classes = useStyles();
+  return (
+     <div className={classes.banner}>
+       <Container className={classes.bannerContent}>
+        <div className={classes.tagline}>
+            <Typography
+             variant="h2"
+             style={{
+                fontWeight: "bold",
+                marginBottom: 15,
+                fontFamily: "Montserrat",
+             }}
+            >
+                Crypto Hunter
+            </Typography>
+            <Typography
+              variant="subtitle2"
+              style={{
+                color: "darkgray",
+                textTransform: "capitalize",
+                fontFamily: "Montserrat",
+              }}
+            >
+                Get all the Info regarding your favorite Crypto currency
+            </Typography>
+        </div>
+       </Container>
+
+     </div>
+  );
+};
+
+export default Banner; 
