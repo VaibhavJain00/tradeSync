389e63c3129de10b77e60974db81f4f894286b12
yashank123 <yashanklakhena@gmail.com>
2023-11-30 23:44:08 +0530

    commit

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCf0IXUWEFLYLHSSg2g1ddvzo16LS95Zko",
+    authDomain: "crypto-hunter-153f2.firebaseapp.com",
+    projectId: "crypto-hunter-153f2",
+    storageBucket: "crypto-hunter-153f2.appspot.com",
+    messagingSenderId: "111317489868",
+    appId: "1:111317489868:web:814271d34b7d4e5e310a69"
+  };
+
+  export default firebaseConfig;
